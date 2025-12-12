d1b37a1373eea066616939c57b4674a4e94148a9
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2025-12-12 16:51:39 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return(
+        <>
+        <h1 className="text-center text-white text-2xl">login</h1>
+        </>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function signupPage() {
+    return(
+        <>
+        <h1 className="text-center text-white text-2xl">signupPage</h1>
+        </>
+    )
+}
